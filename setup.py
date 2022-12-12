630b2ffce31b11bf614f85f75e884206416af486
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-12 16:47:16 -0500

    chore: updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="postgres-database-utils",
-    version="0.1.0",
+    version="0.1.1",
     author="apinanyogaratnam",
     author_email="apinanapinan@icloud.com",
     description="A package for postgres database utilities",
